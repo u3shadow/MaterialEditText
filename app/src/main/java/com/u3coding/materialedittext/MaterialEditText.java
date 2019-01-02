565bfb46c316b5ad8ab221bbb8c13c0ab63efb30
u3shadow <u3shadow@gmail.com>
2019-01-02 13:41:31 +0800

    draw clear button

--- app/src/main/java/com/u3coding/materialedittext/MaterialEditText.java
+++ app/src/main/java/com/u3coding/materialedittext/MaterialEditText.java
@@ -1,42 +1,68 @@
 package com.u3coding.materialedittext;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.EditText;
 
 public class MaterialEditText extends EditText {
     private boolean hideUnderLine;
     private int bottomSpacing;
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private boolean showCleanButton = true;
+    private int iconOuterWidth;
+    private int iconOuterHeight;
 
     public MaterialEditText(Context context) {
         super(context);
+        init();
     }
 
     public MaterialEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MaterialEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
     private void init(){
+        iconOuterHeight = getPiexl(32);
+        iconOuterWidth = getPiexl(48);
         bottomSpacing = getResources().getDimensionPixelSize(R.dimen.inner_components_spacing);
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
         setBackground(null);
         //获取一些位置信息
         int lineStartY = getScrollY()+getHeight()-getPaddingBottom();
         int startX = getScrollX()+getPaddingLeft();
         int endX = getScrollX()+getWidth()-getPaddingRight();
+        if(hasFocus()&&isEnabled()&&!TextUtils.isEmpty(getText())&&showCleanButton){
+            paint.setAlpha(255);
+            int buttonLeft;
+            if (isRTL()){
+                buttonLeft = startX;
+            }else{
+                buttonLeft = endX - iconOuterWidth;
+            }
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.met_ic_clear);
+            buttonLeft += (iconOuterWidth - bitmap.getWidth())/2;
+            int iconTop = lineStartY+bottomSpacing - iconOuterHeight + (iconOuterHeight - bitmap.getHeight())/2;
+            canvas.drawBitmap(bitmap,buttonLeft,iconTop,paint);
+        }
         if(!hideUnderLine){//绘制下划线
             //获取位置
             lineStartY += bottomSpacing;
@@ -57,11 +83,19 @@ public class MaterialEditText extends EditText {
                 canvas.drawRect(startX,lineStartY,endX,lineStartY+6,paint);
             }
         }
-
-
+        super.onDraw(canvas);
     }
-
-
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public boolean isRTL(){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1){
+            return false;
+        }
+        Configuration config = getResources().getConfiguration();
+        return config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL;
+    }
+    private int getPiexl(int dp){
+        return Density.dp2px(getContext(),dp);
+    }
     private boolean isInternalValid() {
         return true;
     }
